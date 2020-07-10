a38a681f079b42990966ff8b6aa3071c4e22686b
vathirajan <vathirajanv95@gmail.com>
2020-07-10 12:41:04 +0530

    initial-commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Person from './Person/Person';
 
 const App = props => {
+
+
+
   //useState always returns exactly two elements
   //first element : current state, an object, whose state needs to be changed
   //second element : is a function which he helps us to update the state 
@@ -26,7 +29,7 @@ const App = props => {
   });
   const [otherState, setOtherState] = useState("Other state")
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     console.log(personState, otherState)
     //When using setState you have to manually override all. 
     //Instead we have multiple individual states so other states are untouched
@@ -35,7 +38,7 @@ const App = props => {
       {
         persons: [
           {
-            name: " Mangathaa Daaw ",
+            name: newName,
             age: 25
           },
           {
@@ -52,18 +55,47 @@ const App = props => {
     )
   }
 
+  const nameChangeHandler = (event) => {
+    setPersonState(
+      {
+        persons: [
+          {
+            name: "Man",
+            age: 25
+          },
+          {
+            name: event.target.value,
+            age: 29
+          },
+          {
+            name: " Thupaaki Daaw ",
+            age: 34
+          }
+        ],
+
+      }
+    )
+  };
+
   return (
     <div className="App" >
       <h1>Hello React Developer</h1>
-      <Person name={personState.persons[0].name} age={personState.persons[0].age} > Alice Name</Person>
-      <Person name={personState.persons[1].name} age={personState.persons[1].age} />
-      <Person name={personState.persons[2].name} age={personState.persons[2].age} />
+      <Person
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
+        click={switchNameHandler.bind(this, "Simran")}> Alice Name</Person>
+      <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+        change={nameChangeHandler} />
+      <Person
+        name={personState.persons[2].name}
+        age={personState.persons[2].age}
+        click={switchNameHandler.bind(this, "Zira")} />
 
-      <button onClick={switchNameHandler}>Corrupt Name</button>
+      <button onClick={() => switchNameHandler("ZIra **")}>Corrupt Name</button>
     </div>
   );
 }
-
-
 
 export default App;
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import './Person.css'
 //In old versions of React's functional component, it didnot support state. 
 //But with Hooks, we could have state inside a functioanl component
 const person = (props) => {
 
     return (
 
-        <div>
-            <p>I'am a Person and I am {props.name} and I am {props.age} years old working in CSS</p>
-            <p>{props.children}</p>
+        <div className="Person">
+            <p onClick={props.click}>I'am a Person and I am {props.name} and I am {props.age} years old working in CSS</p>
+            <p >{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name}></input>
         </div>)
 }
 export default person;
